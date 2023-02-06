b0ec646633be4de5e02aaed7bb7ef04e35b7c840
CabIsMe <dangbac872001@gmail.com>
2023-02-06 14:41:11 +0700

    spring security

--- query.sql
+++ query.sql
@@ -1,2 +1,12 @@
 ALTER TABLE account
 DROP COLUMN ma_so;
+
+ALTER TABLE gia_nuoc_theo_thang
+ALTER COLUMN gia_nuoc float;
+
+-- Vô Table "account" --> Design --> Xóa Column "ma_so"
+-- Vô Diagram --> Tìm table "account", kéo Column "username" vào Table "sinh_vien", Column "MSSV",
+-- Table "account", kéo Column "username" vào Table "quan_tri_vien", Column "MSCB".
+
+ALTER TABLE account
+ALTER COLUMN password varchar(MAX)
