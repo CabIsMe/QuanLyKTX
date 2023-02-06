d544cc3cff4f997fc6cb3e1027c5ae052cf51753
hoductrung1211 <88882681+hoductrung1211@users.noreply.github.com>
2023-02-06 11:35:28 +0700

    Update Table "account"

--- query.sql
+++ query.sql
@@ -3,3 +3,7 @@ DROP COLUMN ma_so;
 
 ALTER TABLE gia_nuoc_theo_thang
 ALTER COLUMN gia_nuoc float;
+
+-- Vô Table "account" --> Design --> Xóa Column "ma_so"
+-- Vô Diagram --> Tìm table "account", kéo Column "username" vào Table "sinh_vien", Column "MSSV",
+-- Table "account", kéo Column "username" vào Table "quan_tri_vien", Column "MSCB".
