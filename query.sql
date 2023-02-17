53986d7941f0a66c85477a4660f4312457efc529
CabIsMe <dangbac872001@gmail.com>
2023-02-17 15:57:39 +0700

    update query

--- query.sql
+++ query.sql
@@ -1,13 +1,7 @@
-ALTER TABLE user
-DROP COLUMN ma_so;
 
-ALTER TABLE gia_nuoc_theo_thang
-ALTER COLUMN gia_nuoc float;
-
--- Vô Table "user" --> Design --> Xóa Column "ma_so"
--- Vô Diagram --> Tìm table "user", kéo Column "username" vào Table "sinh_vien", Column "MSSV",
--- Table "user", kéo Column "username" vào Table "quan_tri_vien", Column "MSCB".
 
 ALTER TABLE users
 ALTER COLUMN password varchar(MAX)
 
+alter table loai_KTX
+add ten_loai nvarchar(50)
